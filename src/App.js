d24a7379b8cca1cea1e7852ff71c49a2c8dad924
Vishal kumar <vishal.oneto11@gmail.com>
2022-12-16 17:16:52 +0530

    till now wallet id accessible

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashConnect } from 'hashconnect';
+
+let accountId =""
+
+let saveData ={
+  topic:"",
+  pairingString:"",
+  privateKey:"",
+  pairedWalletData:null,
+  pairedAccounts:[]
+
+}
+
+let appMetadata = {
+  name: "Nft marketplace oneto11",
+  description: "nft marketplace oneto11",
+  icon: "https://absolute.url/to/icon.png",
+
+}
+
+let hashconnect = new HashConnect();
+
+const connectWallet = async()=>{
+  
+  let initData = await hashconnect.init(appMetadata);
+  saveData.privateKey =initData.privateKey
+
+  let state = await hashconnect.connect()
+  saveData.topic = state.topic
+
+  console.log('\nTopic is : ${saveData.topic} ')
+
+
+  saveData.pairingString = hashconnect.generatePairingString(state,"testnet",false)
+
+  const result = hashconnect.findLocalWallets()
+
+  console.log(result +'result')
+
+  hashconnect.connectToLocalWallet(saveData.pairingString)
+
+
+  hashconnect.pairingEvent.once(pairingData =>{
+    pairingData.accountIds.forEach(id=>{
+      accountId=id
+      console.log(accountId)
+    })
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>hashpack demo connection</h1>
+        <button onClick={connectWallet}>Connect To Hashpack</button>
     </div>
   );
 }
